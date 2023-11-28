f9704a97a55f745e2001b9d80009b22783400359
arogers <micheknows@gmail.com>
2023-11-28 15:24:35 -0700

    Adding grab special days code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-#import os
-# from chatgpt import Assistant
+import os
 
+from openai import OpenAI
+os.environ["OPENAI_API_KEY"] = "sh-Jfb3Wx23UyyNaBTxtMdaT3BlbkFJ600pLLfHjKBOuNLErH2e"
 # Set your ChatGPT API Key
-#API_KEY = os.getenv("sh-Jfb3Wx23UyyNaBTxtMdaT3BlbkFJ600pLLfHjKBOuNLErH2e")
 
 # Create the assistant
 #assistant = Assistant(API_KEY)
@@ -13,8 +13,29 @@
 #print("hi")
 # app.py
 
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+
+
+
+def get_special_days(month, grade):
+
+    prompt = f"Give a list of all special days for the month of {month}.  Include major holidays, minor special days and everything in between.  The special days should be topics of interest to {grade} grade level students."
+    print(prompt)
+
+    client = OpenAI(
+        api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
+    )
+
+    completion = client.completions.create(model="gpt-4", prompt=prompt)
+    reply = completion.choices[0].text
+    print(reply)
+    print(dict(completion).get('usage'))
+    print(completion.model_dump_json(indent=2))
+
+    return reply
 
 @app.route('/')
 def index():
@@ -25,9 +46,23 @@ def createtpt():
     return render_template('tpt/createtpt.html')
 
 
-@app.route('/tpt/createraces')
+
+
+@app.route("/tpt/createraces", methods=['POST', 'GET'])
 def createraces():
-    return render_template('tpt/createraces.html')
+    print('going here')
+    if request.method == 'POST':
+        print('step1')
+        month = request.form.get('month', '')
+        grade = request.form.get('gradeLevel', '')
+        print(month)
+        print(grade)
+        response = get_special_days(month, grade)
+        print(response)
+
+        return jsonify({'response': response})
+    else:
+        return render_template('/tpt/createraces.html')
 
 
 if __name__ == '__main__':
