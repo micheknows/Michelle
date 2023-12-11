f1c1dedfe6d52b01ce129aaba243a3cd910ed2cc
miche <micheknows@gmail.com>
2023-12-11 07:24:21 -0700

    Added key without sending key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from openai import OpenAI
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = os.environ.get("ChatGPTAPI")
 # Set your ChatGPT API Key
 
 # Create the assistant
@@ -21,19 +21,18 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 
 def get_special_days(month, grade):
+    prompt = f"Give a list of all special days for the month of {month}. Include major holidays, minor special days and everything in between. The special days should be topics of interest to {grade} grade level students.  Give the days without any lead in text and format it as jsoon or a dictionary with day:date."
 
-    prompt = f"Give a list of all special days for the month of {month}.  Include major holidays, minor special days and everything in between.  The special days should be topics of interest to {grade} grade level students."
-    print(prompt)
-
-    client = OpenAI(
-        api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
+    client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+    completion = client.chat.completions.create(
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        model="gpt-3.5-turbo",
     )
-
-    completion = client.completions.create(model="gpt-4", prompt=prompt)
-    reply = completion.choices[0].text
-    print(reply)
-    print(dict(completion).get('usage'))
-    print(completion.model_dump_json(indent=2))
+    #reply = completion['choices'][0]['message']['content']
+    reply = completion.choices[0].message.content
+    print("Original reply is:  " + str(reply))
 
     return reply
 
