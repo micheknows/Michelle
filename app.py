137c415e17c52fcdd7ca203bc03ee70d658b60b9
miche <micheknows@gmail.com>
2023-12-11 22:28:28 -0700

    fixed some updatedays errors

--- app.py
+++ app.py
@@ -17,35 +17,6 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 
 
-def get_special_days(month, grade):
-    if IS_LOCAL:
-        # Use dummy data
-        data =  {
-            "month": "December",
-            "days": {
-                "Christmas": "December 25",
-                "Christmas Eve": "December 24"
-            }
-        }
-        json_data = json.dumps(data)
-
-        return Response(json_data, mimetype='application/json')
-    else:
-        prompt = f"Give a list of all special days for the month of {month}. Include major holidays, minor special days and everything in between. The special days should be topics of interest to {grade} grade level students.  Give the days without any lead in text and format it as jsoon or a dictionary with day:date."
-
-        client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
-        completion = client.chat.completions.create(
-            messages=[
-                {"role": "user", "content": prompt}
-            ],
-            model="gpt-3.5-turbo",
-        )
-        reply = completion.choices[0].message.content
-        print("Original reply is:  " + str(reply))
-        logger.info("Original reply is:  " + str(reply))
-
-        return reply
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -87,7 +58,7 @@ def get_special_days():
         print("It's NOT LOCAL!!!")
         if month not in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']:
             month="December"
-        prompt = f"Give a list of all special days for the month of {month}. Include major holidays, minor special days and everything in between. The special days should be topics of interest to {grade} grade level students.  Give the days without any lead in text and format it as jsoon or a dictionary with day:date."
+        prompt = f"Give a list of all special days for the month of {month}. Include major holidays, minor special days and everything in between. The special days should be topics of interest to {grade} grade level students.  Give the days without any lead in text and format it as jsoon or a dictionary with date:day."
 
         client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
         completion = client.chat.completions.create(
@@ -98,11 +69,16 @@ def get_special_days():
         )
         reply = completion.choices[0].message.content
         days = reply
-        print(days)
+
+        print("Original reply is:  " + str(reply))
 
 
 
-    return jsonify(days)
+    return days
+
+
+
+
 
 
 if __name__ == '__main__':
