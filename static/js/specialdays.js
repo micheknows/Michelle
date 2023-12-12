c3058069546763beda1ccc605c8c34de949bfb1c
miche <micheknows@gmail.com>
2023-12-11 22:13:38 -0700

    fixed some updatedays errors

--- static/js/specialdays.js
+++ static/js/specialdays.js
@@ -1,19 +1,18 @@
 let daysList;
 let monthName;
-export let specialdays = {};
+
+// Ensure that specialdays is a property of the window object and not overwritten elsewhere
+window.specialdays = window.specialdays || {};
 
 document.addEventListener('DOMContentLoaded', () => {
 
-daysList = document.getElementById('daysList');
+  daysList = document.getElementById('daysList');
+  monthName = document.getElementById('monthName');
 
-monthName = document.getElementById('monthName');
-
-
-document.getElementById('getDaysBtn').addEventListener('click', () => {
-
-  fetch('/api/specialdays', {
-    method: 'POST'
-  })
+  document.getElementById('getDaysBtn').addEventListener('click', () => {
+    fetch('/api/specialdays', {
+      method: 'POST'
+    })
     .then(response => response.json())
     .catch(err => {
       console.log(err)
@@ -22,80 +21,53 @@ document.getElementById('getDaysBtn').addEventListener('click', () => {
     .then(data => {
       // Display data
       monthName.textContent = data.month;
-
-      updateDaysList(data.days);
+      window.specialdays.updateDaysList(data.days);
     });
+  });
 
-});
+  // Define the updateDaysList function as a method of specialdays
+  window.specialdays.updateDaysList = function(days) {
+    daysList.innerHTML = '';
 
+    for (const day in days) {
+      const li = document.createElement('li');
+      li.textContent = `${day} (${days[day]})`; // Updated to show day and date
+      daysList.appendChild(li);
+    }
+  };
 
+  // Edit day
+  daysList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI') {
+      let day = e.target.textContent;
 
+      e.target.innerHTML = `
+        <input type="text" value="${day}" id="editInput">
+        <button onclick="saveEdit('${day}', this)">Save Edit</button>
+      `;
 
-window.specialdays = {};
+      document.getElementById("editInput").focus();
+    }
+  });
 
-specialdays.updateDaysList = function(days)  {
-  daysList = document.getElementById('daysList');
-  daysList.innerHTML = '';
+  function saveEdit(oldDay, el) {
+    const newDay = el.previousElementSibling.value;
+    el.parentElement.textContent = newDay;
+  }
 
-  for (const day in days) {
+  // Delete day
+  daysList.addEventListener('click', (e) => {
+    if(e.target.className === 'deleteBtn') {
+      e.target.parentElement.remove();
+    }
+  });
+
+  // Add day
+  document.getElementById('addDayBtn').addEventListener('click', () => {
+    const day = prompt('Enter special day:');
     const li = document.createElement('li');
     li.textContent = day;
     daysList.appendChild(li);
-  }
-
-}
-
-daysList = document.getElementById('daysList');
-
-// Edit day
-daysList.addEventListener('click', (e) => {
-  if(e.target.tagName == 'LI') {
-
-    let day = e.target.textContent;
-
-    e.target.innerHTML = `
-      <input type="text" value="${day}" id="editInput">
-      <button onclick="saveEdit('${day}', this)">Save Edit</button>
-    `;
-
-    document.getElementById("editInput").focus();
-
-  }
-});
-
-function saveEdit(oldDay, el) {
-
-  const newDay = el.previousElementSibling.value;
-
-  el.parentElement.innerHTML = newDay;
-
-}
-
-
-// Delete day
-daysList.addEventListener('click', (e) => {
-
-  if(e.target.className == 'deleteBtn') {
-
-    const day = e.target.previousElementSibling.textContent;
-
-    e.target.parentElement.remove();
-
-  }
-
-});
-
-
-// Add day
-document.getElementById('addDayBtn').addEventListener('click', () => {
-
-  const day = prompt('Enter special day:');
-
-  const li = document.createElement('li');
-  li.textContent = day;
-
-  daysList.appendChild(li);
-
-});
+  });
 
 });
